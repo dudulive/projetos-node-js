a1b62fb55e2d11b690c2da27be7d223922cc5ca4
dudulive <edurodriguesbraz@hotmail.com>
2018-08-11 12:54:32 -0300

    subindo os projetos

---
+++ casadocodigo/app/routes/produtos.js
@@ -0,0 +1,52 @@
+module.exports = function(app){
+
+    app.get('/produtos', function(req, res, next){
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.lista(function(erros, resutados){
+            if(erros){
+                return next(erros);
+            }
+            res.format({
+                html: function(){
+                    res.render('produtos/lista', {lista:resultados});
+                },
+                json: function(){
+                    res.json(resultado);
+                }
+            });
+        });
+        connection.end();    
+    });
+
+    app.get('/produtos/form',function(req,res){
+        res.render('produtos/form',
+        {errosValidacao: {}, produto: {}})
+    });
+
+    app.post('/produtos/salva',function(req,res){
+
+        var produto = req.body;
+        req.assert('titulo','Titulo é obrigatório').notEmpty()
+        req.assert('preco','Formato inválido').isFloat();
+
+        var erros = req.validationErrors();
+
+        if(erros){
+            res.format({
+                html: function(){
+                    res.status(400).render('produtos/form',{errosValidacao:erros,produto:produto});
+                },
+                json: function(){
+                    res.status(400).json(erros);
+                }
+            });
+        }
+        
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto,function(erros,resultados){
+            res.redirect('/produtos');
+        });    
+    });
+}
